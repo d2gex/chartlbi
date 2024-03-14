c36bb344002d2d65e52324668e9a40fdfe6b69c4
d2gex <daniel.garcia@d2garcia.com>
2024-03-14 12:34:03 +0100

    Refactor lbi algorithm class to split lbi and run methods

--- R/lbi_algo.R
+++ R/lbi_algo.R
@@ -3,25 +3,42 @@ library("R6")
 LbiAlgo <- R6Class("LbiAlgo", public = list(
   data = NULL,
   params = NULL,
-
+  # @formatter:off
   #' Initialise the LBI class
   #'
   #' @param data length-frequency dataframe
   #' @param params R6 object with biological parameters and binwidth
-  initialize = function (data, params) {
+  # @formatter:on
+  initialize = function(data, params) {
     self$data <- data
     self$params <- params
   },
+  lbi = function() {
+    return(lb_ind_pretty(self$data,
+                         self$params$binwidth,
+                         self$params$linf,
+                         self$params$l50))
+  },
 
+  # @formatter:off
   #' Run LBI algorithm.
   #'
-  #' @returns A dataframe with all calculated indicators.
-  run = function () {
-    return (
-      lb_ind_pretty(self$data,
-                    self$params$binwidth,
-                    self$params$linf,
-                    self$params$l50)
+  #' @returns A list with a vector of years and dataframe holding all calculated indicators
+  # @formatter:on
+  run = function() {
+    results <- self$lbi()
+    output <- data.frame(
+      Lc_Lmat = results$Lc_Lmat,
+      L25_Lmat = results$L25_Lmat,
+      Lmax5_Linf = results$Lmax5_Linf,
+      Pmega = results$Pmega,
+      Lmean_Lopt = results$Lmean_Lopt,
+      Lmean_Lfem = results$Lmean_LFeM
     )
+
+    return(list(
+      years = results$Year,
+      estimates = output))
+
   }
 ))
--- tests/testthat/test-lbi_algo.R
+++ tests/testthat/test-lbi_algo.R
@@ -5,7 +5,7 @@ test_that("procedural LBI result == OOP LBI result", {
   params$binwidth <- 1
 
   lbi_engine <- LbiAlgo$new(freq_df_sample, params)
-  oop_result <- lbi_engine$run()
+  oop_result <- lbi_engine$lbi()
   procedural_result <- lb_ind_pretty(freq_df_sample, 1, 3, 2)
   expect_equal(oop_result, procedural_result)
 })
