ffd61a0445b26e1afe92baec9b9add787cae04c5
d2gex <daniel.garcia@d2garcia.com>
2024-03-14 09:27:29 +0100

    Add multiple freq matrix producer to the testing suite

--- tests/testthat/helper-lbi-freq_wal-matrices.R
+++ tests/testthat/helper-lbi-freq_wal-matrices.R
@@ -1,4 +1,4 @@
-freq_df <- data.frame(
+freq_df_sample <- data.frame(
   meanlength = c(1, 2, 3, 4),
   X2021 = c(NA, 2, 3, 3),
   X2022 = c(2, 3, 3, 2),
@@ -6,10 +6,30 @@ freq_df <- data.frame(
   X2024 = c(4, 1, NA, 3)
 )
 
-wal_df <- data.frame(
+wal_df_sample <- data.frame(
   meanlength = c(1, 2, 3, 4),
   X2021 = c(4.3, 5.4, 6.6, 8),
   X2022 = c(4.4, 5.9, 6.6, 8),
   X2023 = c(4.3, 5.8, 7, 14),
   X2024 = c(4.5, 5.1, 6.9, 12)
 )
+
+multiple_freq_df_producer <- function (freq_df, num_times) {
+
+  freq_df_list <- list()
+  for (i in 1:num_times) {
+    df <- freq_df
+    col_or_row <- sample(1:2, 1)
+    values <- sample(1:100, 4)
+    if (col_or_row == 1) {
+      position <- sample(2:length(df), 1)
+      df[,position] <- values
+    }
+    else {
+      position <- sample(1:nrow(df), 1)
+      df[position, -1] <- values
+    }
+    freq_df_list[[i]] <- df
+  }
+  return (freq_df_list)
+}
--- tests/testthat/test-ices_lbi_utilities.R
+++ tests/testthat/test-ices_lbi_utilities.R
@@ -1,27 +1,20 @@
 test_that("ICES and Pretty versions reports the same results", {
 
   # Generate 100 different length-frequency matrices to ensure the two functions are well tested
-  for (i in 1:100) {
-    col_or_row <- sample(1:2, 1)
-    values <- sample(1:100, 4)
-    if (col_or_row == 1) {
-      position <- sample(2:length(freq_df), 1)
-      freq_df[,position] <- values
-    }
-    else {
-      position <- sample(1:nrow(freq_df), 1)
-      freq_df[position, -1] <- values
-    }
-    result_ices <- lb_ind_ices(data = freq_df,
+  freq_dfs <- multiple_freq_df_producer(freq_df_sample, 100)
+  for (i in 1:length(freq_dfs)) {
+    result_ices <- lb_ind_ices(data = freq_dfs[[i]],
                                binwidth = 1,
                                linf = 3,
                                lmat = 2,
-                               weight = wal_df)
-    result_pretty <- lb_ind_pretty(freq_df,
+                               weight = wal_df_sample)
+    result_pretty <- lb_ind_pretty(freq_dfs[[i]],
                                    binwidth = 1,
                                    linf = 3,
                                    lmat = 2)
-    expect_equal(result_ices[, !colnames(result_ices) %in% c('Lmaxy_Lopt', 'Lmaxy')], result_pretty)
+    trimmed_result_ices <- result_ices[, !colnames(result_ices) %in% c('Lmaxy_Lopt', 'Lmaxy')]
+    expect_equal(trimmed_result_ices, result_pretty)
+
   }
 })
 
